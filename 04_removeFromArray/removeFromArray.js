7389540285b12490855c68b89011ae271775c4e1
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2024-10-03 16:14:31 +0100

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    let new_arr = [];
+    arr.forEach(element => {
+        if(values.indexOf(element) == -1) new_arr.push(element);
+    });
+    // console.log(new_arr);
+    return new_arr;
 };
 
 // Do not edit below this line
