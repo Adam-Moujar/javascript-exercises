668454f4fe9242655fd2e273d4b788cdfddf7946
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2024-10-03 16:21:32 +0100

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0) return "ERROR";
+    if(typeof a != "number" || typeof b != "number") return "ERROR";
+    if(a % 1 != 0 || b % 1 != 0) return "ERROR";
+    let sum = 0;
+    const min = function(a,b){
+        if(a < b) return a;
+        return b;
+    }
+    const max = function(a,b){
+        if(a > b) return a;
+        return b;
+    }
+    for(let i = min(a,b); i <= max(a,b); i++) sum = sum + i;
+    return sum;
 };
 
 // Do not edit below this line
